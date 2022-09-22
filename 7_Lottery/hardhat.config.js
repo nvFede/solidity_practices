2b949d6c90fde6ca3d1f8110063496435ce5deed
Federico Noya <nvfede@gmail.com>
2022-09-22 13:24:04 -0300

    last update

--- 7_Lottery/hardhat.config.js
+++
@@ -1,7 +0,0 @@
-require("@nomiclabs/hardhat-waffle");
-require('@nomicfoundation/hardhat-toolbox');
-
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.17",
-};
